0573d7e118c33f4e10c95debab6aa093dd2f4e39
v.stupakov <v.stupakov@samsung.com>
2017-11-17 12:34:37 +0200

    Removed excell generation. Transposed image

--- InterferenceAnalysis.py
+++ InterferenceAnalysis.py
@@ -15,23 +15,16 @@ print("Script started!")
 
 image_path = os.path.abspath("images")
 plots_path = os.path.abspath("plots")
-excel_path = os.path.abspath("excels")
 
 shutil.rmtree(plots_path, ignore_errors=True)
 os.mkdir(plots_path)
-
-shutil.rmtree(excel_path, ignore_errors=True)
-os.mkdir(excel_path)
-
-excel_filename = os.path.join(excel_path, "all_excel.xlsx")
-excel_writer = pd.ExcelWriter(excel_filename, engine='xlsxwriter')
 
 for image_name in os.listdir(image_path):
 
     image = cv.imread(os.path.join(image_path, image_name), cv.IMREAD_GRAYSCALE).astype(np.float32)
     size = (500, 500)
     image = cv.resize(image, size)
-    image = cv.normalize(image, image, 0, 1, cv.NORM_MINMAX)
+    image = cv.normalize(image, image, 0, 1, cv.NORM_MINMAX).T
 
     data = [go.Surface(z=image)]
 
@@ -51,9 +44,5 @@ for image_name in os.listdir(image_path):
 
     plot_filename = os.path.join(plots_path, "plot_" + image_name.rstrip(".")) + ".html"
     plot(fig, filename=plot_filename, auto_open=False)
-
-    pd.DataFrame(image).to_excel(excel_writer=excel_writer, sheet_name=image_name)
-
-excel_writer.save()
 
 print("Script finished!")
